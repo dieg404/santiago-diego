c8f91c3fa4e38a75374e4c063391bc589c66af9b
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-04-08 11:36:45 -0500

    3

--- segundo_taller.py
+++ segundo_taller.py
@@ -1,4 +1,17 @@
 # primera funcion: Mostrar el menú de las opciones de lista
+<<<<<<< HEA 
+def ml(): #ml -> "Menú Lista"
+   print('1. "Imprimir menú de las opciones de lista.')
+   print('2. "Contar los elementos totales que tiene una lsita.')
+   print('3. "Mostrar un elemento de una posicione en especifica de la lista."')
+   print('4. "Verificar si la variable ingresada está en la lista."')
+   print('5. "Mostrar todos los elemntos de la lista."')
+   print('6. "Agregar nuevo elemento a una lista."')
+   print('7. "Sacar un elemento especifico de la lista."')
+   print('8. "Sacar un elemento de una posicion en especifico."')
+   print('9. "Concantenar dos listas."')
+   print('10. "Remplazar algun elemento de la lista."')    
+=======
 def ml(): #ml -> "Menú Lista"
  print('1. "Imprimir menú de las opciones de lista.')
  print('2. "Contar los elementos totales que tiene una lsita.')
@@ -10,11 +23,15 @@ def ml(): #ml -> "Menú Lista"
  print('8. "Sacar un elemento de una posicion en especifico."')
  print('9. "Concantenar dos listas."')
  print('10. "Remplazar algun elemento de la lista."')    
+>>>>>>> 19c25b29013e5677fcca9fa5903f53c7e6fad701
 
 # segunda funcion
 def contador(lista):
     return len(lista)
 
+# segunda funcion
+def contador(lista):
+    return len(lista)
 
 #tercera funcion
 def posicion(lista,n):
@@ -27,9 +44,34 @@ def posicion(lista,n):
        return "Posicion fuera de rango"
         
 
+<<<<<<< HEAD
+#tercera funcion
+def posicion(lista):
+    i=0
+    ubicacion=1
+    n=int(input("Ingrese el numero de posicion que desea saber: "))
+    while (i==n):
+        for i in lista:
+            print(i)
+    i+=1
+=======
 # Cuarta funcion: Verificar si la variable ingresada está en la lista
+<<<<<<< HEAD
+>>>>>>> 19c25b29013e5677fcca9fa5903f53c7e6fad701
+=======
 print("cambie algo hp hagalo mlp lo reto")
+>>>>>>> 90b64283d381341d7ee339346efce41377ef0128
+
+
+
+<<<<<<< HEAD
+# Cuarta funcion: Verificar si la variable ingresada está en la lista
+def ve(variable
+=======
 
 
 
 
+print ("sapohpta")
+
+>>>>>>> 90b64283d381341d7ee339346efce41377ef0128
