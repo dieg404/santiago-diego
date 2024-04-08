38c4cef851c67b7843e3cfa36a5d3f03b4f1e96c
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-04-08 10:23:37 -0500

    Add files via upload

---
+++ segundo_taller.py
@@ -0,0 +1,15 @@
+# primera funcion: Mostrar el menú de las opciones de lista
+def ml():
+ print('1. "Imprimir menú de las opciones de lista.')
+ print('2. "Contar los elementos totales que tiene una lsita.')
+ print('3. "Mostrar un elemento de una posicione en especifica de la lista."')
+ print('4. "Verificar si la variable ingresada está en la lista."')
+ print('5. "Mostrar todos los elemntos de la lista."')
+ print('6. "Agregar nuevo elemento a una lista."')
+ print('7. "Sacar un elemento"')    
+
+
+
+
+
+
