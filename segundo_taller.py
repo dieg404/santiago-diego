7fb6b09ebcf18ed5a00c81cf7de5551231928b87
Flowi2118 <166416453+Flowi2118@users.noreply.github.com>
2024-04-09 11:01:49 -0500

    11

--- segundo_taller.py
+++ segundo_taller.py
@@ -1,12 +1,9 @@
 import random
 
 # primera funcion: Mostrar el menú de las opciones de lista
-def ml(x): #ml -> "Menú Lista"
-   if x==1:
-    print('1. "Contar los elementos totales que tiene una lista.')
-    print(f"El numero de elementos que se encuentran en la lista A son {contador(lista_a)}")
-   elif x==2:
-    print('2. "Mostrar un elemento de una posicione en especifica de la lista."')
+def ml(): #ml -> "Menú Lista"
+   print('1. "Contar los elementos totales que tiene una lista.')
+   print('2. "Mostrar un elemento de una posicione en especifica de la lista."')
    print('4. "Verificar si la variable ingresada está en la lista."')
    print('5. "Mostrar todos los elemntos de la lista."')
    print('6. "Agregar nuevo elemento a una lista."')
@@ -17,17 +14,17 @@ def ml(x): #ml -> "Menú Lista"
 
 # segunda funcion: Contar cuantos elementos se encunetran en una lista
 def contador(lista_a):
-    return len(lista_a)
+   return len(lista_a)
 
 #tercera funcion: Identificar en que posicion se encuentra un elemento
-def posicion(lista,n):
+def posicion(lista_a,n):
    i=0
-   if n<= len(lista):
-        for i in range(len(lista)):
+   if n<= len(lista_a):
+        for i in range(len(lista_a)):
             if i==n:
-              return lista[n]
+              return f"fue encontrando en posicion {lista_a[n]}"
    else:
-       return "Posicion fuera de rango"
+       return "se encuentra fuera de rango"
         
 # Cuarta funcion: Verificar si la variable ingresada está en la lista
 def elemento(lista,m):
@@ -67,9 +64,9 @@ def eliminacion_posicion(lista,x):
       return "se encuentra fuera del rango"
 
 # Novena Funcion: Concatenacion de dos listas
-def concatenar(lista,lista_b):
-   lista.extend(lista_b)
-   return lista
+def concatenar(lista_a,lista_b):
+   lista_a.extend(lista_b)
+   return lista_a
 
 # Decima Funcion: Remover elemento y agregar elemento nuevo
 def remover_añadir(lista,a):
@@ -81,6 +78,17 @@ def remover_añadir(lista,a):
     return lista
    else:
       return "Posicion fuera de rango"
+   
+# Onceava Funcion: Implementacion de todas las funciones
+def implementacion(x):
+   if x==1:
+      print(f"El numero de elementos que se encuentran en la lista A son {contador(lista_a)}")
+   if x==2:
+      n=int(input("Ingrese la posicion del elemento que desea saber: "))
+      print(f"El elemento {n} {posicion(lista_a,n)}")
+
+
+
 
 # Inicio de codigo
 lista_a=[]
@@ -95,4 +103,7 @@ for i in range(b+1):
    x=random.randint(1,20)
    lista_b.append(x)
 #Fin de bucles creacion de listas
-           
+
+ml()
+x=int(input("Ingrese la opcion que desea: ")) 
+implementacion(x)          
